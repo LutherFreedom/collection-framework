3cec7aa4f111017d9a1647a64a220a2550536dca
luther <maseratipp@gmail.com>
2019-03-29 11:02:42 +0800

    AbstractList

--- src/main/java/com/luther/read/AbstractListDemo.java
+++ src/main/java/com/luther/read/AbstractListDemo.java
@@ -5,6 +5,7 @@ import com.luther.read.po.Person;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * PackageName: com.luther.read
@@ -17,11 +18,11 @@ public class AbstractListDemo {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         ArrayList<Person> personArrayList = new ArrayList<>();
         personArrayList.add(new Person("AAA", "F"));
-        personArrayList.add(new Person("AAA", "F"));
+        personArrayList.add(new Person("BBB", "F"));
 
-        personArrayList.add(new Person("AAA", "F"));
+        personArrayList.add(new Person("CCCC", "F"));
 
-        personArrayList.add(new Person("AAA", "F"));
+        personArrayList.add(new Person("DDD", "F"));
 
         Class clazz = ArrayList.class;
         Field[] fields = clazz.getDeclaredFields();
@@ -32,5 +33,13 @@ public class AbstractListDemo {
         modCountField.setAccessible(true);
         System.out.println(modCountField.get(personArrayList));
         System.out.println("1111");
+
+        ListIterator<Person> personIterator = personArrayList.listIterator();
+        personIterator.next();
+        personIterator.set(new Person("EEE", "m"));
+        personIterator.add(new Person("FFF", "F"));
+
+
+        System.out.println(personIterator.nextIndex());
     }
 }

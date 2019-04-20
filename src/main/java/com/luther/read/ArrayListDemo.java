f892eb770f51e9826e2bdea3ade98693015a3173
luther <maseratipp@gmail.com>
2019-04-20 14:24:32 +0800

    SubList的验证

--- src/main/java/com/luther/read/ArrayListDemo.java
+++ src/main/java/com/luther/read/ArrayListDemo.java
@@ -1,5 +1,8 @@
 package com.luther.read;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * PackageName: com.luther.read
  *
@@ -8,4 +11,25 @@ package com.luther.read;
  * TODO:
  */
 public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String> rootList = new ArrayList<String>(){
+            {
+                add("AAAA");
+                add("BBBB");
+                add("CCCC");
+                add("DDDD");
+            }
+        };
+        printList(rootList);
+
+        List<String> subList = rootList.subList(1,3);
+        subList.set(0, "EEEE");
+        printList(subList);
+        printList(rootList);
+    }
+
+    static void printList(List<String> list){
+        System.out.println("Start println");
+        list.forEach(System.out::println);
+    }
 }

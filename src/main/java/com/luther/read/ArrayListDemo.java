47ede148b60aa76763698126c8823d3b50eeaec9
luther <maseratipp@gmail.com>
2019-04-30 09:26:25 +0800

    ArrayList的基础方法测试

--- src/main/java/com/luther/read/ArrayListDemo.java
+++ src/main/java/com/luther/read/ArrayListDemo.java
@@ -1,5 +1,7 @@
 package com.luther.read;
 
+import com.luther.read.po.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +14,74 @@ import java.util.List;
  */
 public class ArrayListDemo {
     public static void main(String[] args) {
+        remove();
+    }
+
+    static void remove(){
+        List<String> rootList = new ArrayList<String>(){
+            {
+                add("AAAA");
+                add("BBBB");
+                add("CCCC");
+                add("DDDD");
+            }
+        };
+
+        List<String> sonList = new ArrayList<String>(){
+            {
+                add("AAAA");
+                add("BBBB");
+                add("EEEE");
+                add("FFFF");
+            }
+        };
+
+        rootList.removeAll(sonList);
+
+    }
+
+    static void toArray(){
+        List<String> rootList = new ArrayList<String>(){
+            {
+                add("AAAA");
+                add("BBBB");
+                add("CCCC");
+                add("DDDD");
+            }
+        };
+
+        String[] a = new String[10];
+        String re[] = rootList.toArray(a);
+        System.out.println(re.length);
+
+        printArr(re);
+    }
+    static void testCopy(){
+        ArrayList<Person> rootList = new ArrayList<Person>(){
+            {
+                add(new Person("Aaaa", "F"));
+                add(new Person("Bbbb", "F"));
+                add(new Person("Cccc", "F"));
+                add(new Person("Dddd", "F"));
+            }
+        };
+        ArrayList<Person>  copyList = (ArrayList<Person>) rootList.clone();
+
+        System.out.println(rootList.get(0) == copyList.get(0));
+    }
+
+    static void testInit(){
+        List<String> list1 = new ArrayList<>();
+
+        List<String> list2 = new ArrayList<>(0);
+
+        List<String> list3 = new ArrayList<>(Integer.MAX_VALUE);
+
+        System.out.println(list1.size());
+        System.out.println(list2.size());
+    }
+
+    static void testSub(){
         List<String> rootList = new ArrayList<String>(){
             {
                 add("AAAA");
@@ -27,9 +97,14 @@ public class ArrayListDemo {
         printList(subList);
         printList(rootList);
     }
-
     static void printList(List<String> list){
         System.out.println("Start println");
         list.forEach(System.out::println);
+    }
+
+    static void printArr(String[] arr){
+        System.out.println("Start println");
+        for (int i = 0; i< arr.length; i++)
+            System.out.println(arr[i]);
     }
 }
